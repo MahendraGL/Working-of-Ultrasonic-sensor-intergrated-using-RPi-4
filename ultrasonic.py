47318042361962cfa5ce8f84c59c9505d508c439
MahendraGL <133708107+MahendraGL@users.noreply.github.com>
2023-06-22 16:10:32 +0530

    Update ultrasonic.py

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,26 +1,31 @@
+#Import nessesary files
 import RPi.GPIO as GPIO
 import time 
 GPIO.setwarnings(False)
 
+#GPIO is set to board numbering mode
 GPIO.setmode(GPIO.BOARD)
 
 trigpin=40
 echopin=38
 LED=36
 
+#Set the GPIO pins as input and/or output as per requirement
 GPIO.setup(trigpin,GPIO.OUT)
 GPIO.setup(echopin,GPIO.IN)
 GPIO.setup(LED,GPIO.OUT)
 
 try:
     while True:
-        
+
+        #Set the triggerpin for 10 microseconds
         GPIO.output(trigpin,0)
         time.sleep(0.2)
         GPIO.output(trigpin,1)
         time.sleep(10E-6)
         GPIO.output(trigpin,0)
-        
+
+        #Measure the travel_time from echopin
         while GPIO.input(echopin)==0:
             starttime=time.time()
             pass
@@ -30,11 +35,14 @@ try:
             pass
         
         t_time=endtime-starttime
+
+        #Calculate the distance from the measured time.
         distance=t_time*17510
         distance=round(distance,2)
         print("Distance in cm is: ",distance)
         time.sleep(0.2)
-        
+
+        #Glow LED for any condition depending on the distance.
         if distance<15:
             GPIO.output(LED,0)
             print("Something detected within 15cm: ")
